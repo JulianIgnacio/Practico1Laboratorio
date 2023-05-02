e29b81b5f6e28a55517f985db75d77df7cc32dba
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-01 23:49:23 -0300

    Realizado Ejercicio 1 tp laboratorio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{ 
+internal class Programa
+{
+    public static void Main()
+    {
+        double precio,precioFinal;
+        int cantidad;
+        Console.WriteLine("Ingrese el precio del producto:");
+        precio = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ingrese la cantidad del producto a llevar:");
+        cantidad = Convert.ToInt32(Console.ReadLine());
+            precioFinal = cantidad * precio;
+        Console.WriteLine("Su precio final es:" + precioFinal);
+        Console.ReadKey();
+    }
+}
+}
