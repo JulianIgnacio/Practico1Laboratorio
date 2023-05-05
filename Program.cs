d6d993d191fc41a9aa11c52cae75fce6c3e20766
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-04 22:08:30 -0300

    Realizando ejercicio 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Programa
+    {
+        public static void Main()
+        {
+            double promedio;
+            int suma;
+            int valor1 = 5; int valor2 = 7; int valor3 = 9; int valor4 = 15;
+            suma = valor1 + valor2 + valor3 + valor4;
+            promedio = suma / 4;
+            Console.WriteLine("Su Suma es:{0} y el promedio es:{1}", suma, promedio);
+            Console.ReadKey();
+        }
+    }
+}
