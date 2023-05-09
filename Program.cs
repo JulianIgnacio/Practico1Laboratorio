9e728857917450a560c559392d551263b5d6c436
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-08 23:54:52 -0300

    realizando ejercicio2 oplog-relac

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2Op_Log_Relac
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 10,b=a,c=15,d=5;
+            bool comp;
+            comp = ((a==b) || (b>c) ||(c<d));
+            Console.WriteLine("El resultado de la comparacion es: {0}",comp);
+            Console.ReadKey();
+        }
+    }
+}
